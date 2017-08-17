00cd4c07ceaf1dc71e7643a25fe3222d6fa0f18c
Jacob Rich <jamr44@gmail.com>
2017-08-17 09:48:49 -0700

    values to be added to prediction feature

--- data/predictions.R
+++ data/predictions.R
@@ -5,6 +5,9 @@ family satisfaction upper
 
 
 
+trash <- which(colnames(total) == "gis_id" | colnames(total) == "x1" | colnames(total) == "block_group")
+new_total <- total[,-trash]
+
 prediction.creator <- function(data, change, var, model){
   col = which(colnames(data)==var) #column to change
   predictions = data.frame(matrix(nrow = 482, ncol = 8))
@@ -14,7 +17,8 @@ prediction.creator <- function(data, change, var, model){
     new.data <- data
     new.vals <- change #change values
     new.data[i,col] <- new.vals #reinsert values
-    predictions[i,] <- lavPredict(model, type = "lv", newdata = new.data[i,]) #predict
+    temp <- lavPredict(model, type = "lv", newdata = scale(new.data)) #predict
+    predictions[i,] <- temp[i,]
   }
   return(predictions)
 }
@@ -22,10 +26,10 @@ prediction.creator <- function(data, change, var, model){
 
 
 final.preds.fun <- function(variable){
-  col<- which(names(total)==variable)
-  set = median(total[,col])
-  set.max = max(total[,col])
-  set.min = min(total[,col])
+  col<- which(names(new_total)==variable)
+  set = median(new_total[,col])
+  set.max = max(new_total[,col])
+  set.min = min(new_total[,col])
   
   new.set <- c(set.min, set*.5, set, set*2, set.max)
   
@@ -37,7 +41,7 @@ final.preds.fun <- function(variable){
   
   
   for(i in new.set){
-      preds <- prediction.creator(data = total, change = i, var = variable, model = fit)
+      preds <- prediction.creator(data = new_total, change = i, var = variable, model = fit)
       preds$gis <- total$gis_id
       
       if(i == new.set[1]){
@@ -64,11 +68,11 @@ final.preds.fun <- function(variable){
 }
 
 
-final.preds.fun("fam_upper")
+final.preds.fun("avg_transit_time")
 
 
 
-home <- read.csv("predictions_median_home_value.csv")
+home <- read.csv("predictions_median_house_value.csv")
 unit <- read.csv("predictions_occupied_housing_units.csv")
 transit <- read.csv("predictions_avg_transit_time.csv")
 fam <- read.csv("predictions_fam_upper.csv")
@@ -88,9 +92,36 @@ write.csv(transit, "predictions_avg_transit_time.csv")
 write.csv(fam, "predictions_fam_upper.csv")
 
 
+variable = "occupied_housing_units"
+col<- which(names(new_total)==variable)
+set = median(new_total[,col])
+set.max = max(new_total[,col])
+set.min = min(new_total[,col])
+new.set.home <- c(set.min, set*.5, set, set*2, set.max)
+new.set.trans <- c(set.min, set*.5, set, set*2, set.max)
+new.set.units <- c(set.min, set*.5, set, set*2, set.max)
+new.set.fam <- c(set.min, set*.5, set, set*2, set.max)
+
+set.new <- data.frame(matrix(nrow = 20, ncol = 4))
+colnames(set.new) <- c("var", "val", "name", "id")
+set.new[1:5,1] <- "median_house_value"
+set.new[1:5,2] <- new.set.home
+set.new[6:10,1] <- "avg_transit_time"
+set.new[6:10,2] <- new.set.trans
+set.new[11:15,1] <- "occupied_housing_units"
+set.new[11:15,2] <- new.set.units
+set.new[16:20,1] <- "fam_upper"
+set.new[16:20,2] <- new.set.fam
+
+temp1 <- c("min", "half_med", "med", "double_med", "max")
+temp1 <- rep(temp1, 4)
+set.new$name <- temp1
 
 
+temp2 <- c(1:5)
+temp2 <- rep(temp2, 4)
+set.new$id <- temp2
 
-
+write.csv(set.new,"prediction_values_ids.csv")
 
 
